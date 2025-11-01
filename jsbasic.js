b944c5a5b1e041525b5dfb12b52fda0403c6a1e6
Tirthpanchal412 <tirthpanchal412@gmail.com>
2025-11-01 11:48:05 +0530

    jsbasic

---
+++ jsbasic.js
@@ -0,0 +1,18 @@
+//console.log("Hello, World");
+
+// let namee = "Tirth";
+// let age = 17;
+
+// // console.log(namee)
+// // console.log(age)
+
+// // console.log(namee , age);
+// // console.table([namee , age]);
+
+// console.log(typeof namee)
+// console.log(typeof age)
+
+let a = 2;
+let b = "2";
+console.log(a == b); //== is for value 
+console.log(a === b); // === is for type
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+//console.log("Hello, World");
+
+let namee = "Tirth";
+let age = 17;
+
+// console.log(namee)
+// console.log(age)
+
+// console.log(namee , age);
+// console.table([namee , age]);
+
+console.log(typeof namee)
